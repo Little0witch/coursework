330af4eb6737afbbd481c667ec7fd1c8331e8043
Little0witch <eeeee.nom@gmail.com>
2023-03-23 14:46:16 +0300

    f

--- Headers/BotHard.h
+++ Headers/BotHard.h
@@ -10,10 +10,14 @@ class BotHard{
     int** enemyField = nullptr;//0 - пустая 1 - удар 2 - корабль
     bool hit;
     int xOfLastHit, yOfLastHit;
+    int sizeOfShips;
     ListOfCoord listOfHit = nullptr;
     ListOfShips listOfMyShips = nullptr;
+    ListOfCoord  listOfCoord = nullptr;
 
 public:
+    BotHard();
+
     ListNodeShips *getListOfMyShips(){
         return listOfMyShips;
     }
@@ -21,5 +25,21 @@ public:
         return myField;
     }
 
-
+    virtual ~BotHard() {
+        freeMemory(enemyField,10);
+        freeMemory(myField,10);
+        freeList(&listOfMyShips);
+        freeList(&listOfHit);
+        freeList(&listOfCoord);
+    }
 };
+
+BotHard::BotHard() {
+    hit = false;
+    enemyField = allocateMemory(enemyField,10,10);
+    init(enemyField,10,10);
+    xOfLastHit = yOfLastHit = -1;
+    myField = autoPositioningOfShips();
+    sizeOfShips = 4;
+    insertListFromFile(&listOfCoord,"/home/user/CLionProjects/coursework/Resources/Txt/PointsFor4");
+}
