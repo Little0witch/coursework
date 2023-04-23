6bb62457e93128057bc3325d5af2481681e21690
tanya <eeeee.nom@gmail.com>
2023-04-24 01:09:17 +0300

    add main/difficulty levels windows

--- main.cpp
+++ main.cpp
@@ -5,52 +5,56 @@
 #include "Headers/ListOfShips.h"
 #include "Headers/BotSoft.h"
 #include "Headers/BotHard.h"
-
+#include "Headers/main_menu.h"
 
 using namespace sf;
 
 int main()
 {
     //RenderWindow window(sf::VideoMode(1846, 1048), "SFML works!");
+   // sf::RenderWindow tmp(sf::VideoMode(1836, 1048),"Battlership");
+    //main_menu window(tmp);
+    main_menu window;
+    window.main_menu_run();
 
-    BotSoft botSoft1;
-    BotHard botHard;
-
-    ListOfShips listOfShips1 = nullptr;
-    ListOfShips listOfShips2 = nullptr;
-
-    listOfShips1 = botSoft1.getListOfMyShips();
-    listOfShips2 = botHard.getListOfMyShips();
-
-    int** field1 = botSoft1.getMyField();
-    int** field2 = botHard.getMyField();
-
-    showArray(field1,10,10);
-    printf("\n");
-    showArray(field2,10,10);
-    printf("\n");
-
-
-    int x,y;
-    int flag = -1;
-    struct coordinates coord{};
-
-    while (sizeList(listOfShips1) > 0)
-    {
-        printf("\nInput x: ");
-        scanf("%d",&x);
-
-
-        do {
-            coord = botHard.giveCoordinates();
-            printf("\nx: %d, y: %d",coord.x,coord.y);
-            flag = isHit(listOfShips1,&listOfShips1,coord.x,coord.y);
-            printf("flag = %d",flag);
-            botHard.statusGame(flag);
-        } while (flag >= 0);
-
-    }
-
-    printf("\n\n\n\nbbbbbbbbb");
+//    BotSoft botSoft1;
+//    BotHard botHard;
+//
+//    ListOfShips listOfShips1 = nullptr;
+//    ListOfShips listOfShips2 = nullptr;
+//
+//    listOfShips1 = botSoft1.getListOfMyShips();
+//    listOfShips2 = botHard.getListOfMyShips();
+//
+//    int** field1 = botSoft1.getMyField();
+//    int** field2 = botHard.getMyField();
+//
+//    showArray(field1,10,10);
+//    printf("\n");
+//    showArray(field2,10,10);
+//    printf("\n");
+//
+//
+//    int x,y;
+//    int flag = -1;
+//    struct coordinates coord{};
+//
+//    while (sizeList(listOfShips1) > 0)
+//    {
+//        printf("\nInput x: ");
+//        scanf("%d",&x);
+//
+//
+//        do {
+//            coord = botHard.giveCoordinates();
+//            printf("\nx: %d, y: %d",coord.x,coord.y);
+//            flag = isHit(listOfShips1,&listOfShips1,coord.x,coord.y);
+//            printf("flag = %d",flag);
+//            botHard.statusGame(flag);
+//        } while (flag >= 0);
+//
+//    }
+//
+//    printf("\n\n\n\nbbbbbbbbb");
     return 0;
 }
