f33f949b139459f60c260df8f0f012043cf5b628
tanya <eeeee.nom@gmail.com>
2023-04-29 23:54:21 +0300

    add button illumination

--- Headers/dif_levels.h
+++ Headers/dif_levels.h
@@ -9,15 +9,33 @@ class dif_levels
 private:
     sf::RenderWindow window;
     sf::Texture background;
-    sf::Texture button_easy;
-    sf::Texture button_hard;
-    sf::Texture button_exit;
-    sf::Sprite sprite;
+    sf::Texture button_easy_blue;
+    sf::Texture button_easy_red;
+    sf::Texture button_hard_blue;
+    sf::Texture button_hard_red;
+    sf::Texture button_exit_blue;
+    sf::Texture button_exit_red;
+
+    sf::Sprite sprite_background;
+    sf::Sprite sprite_button_easy_blue;
+    sf::Sprite sprite_button_easy_red;
+    sf::Sprite sprite_button_hard_blue;
+    sf::Sprite sprite_button_hard_red;
+    sf::Sprite sprite_button_exit_blue;
+    sf::Sprite sprite_button_exit_red;
 public:
     dif_levels() : window(sf::VideoMode(1846,1048),"Battlership")
     {
-        background.loadFromFile("../Resources/Img/Difficulty levels/difficulty_levels.png");
-        sprite.setTexture(background);
+        background.loadFromFile("../Resources/Img/Difficulty levels/win_without_button.png");
+        button_hard_red.loadFromFile("../Resources/Img/Difficulty levels/button_hard.png");
+        button_hard_blue.loadFromFile("../Resources/Img/Difficulty levels/button_hard_blue.png");
+        sprite_background.setTexture(background);
+
+        sprite_button_hard_blue.setTexture(button_hard_blue);
+        sprite_button_hard_blue.setPosition(570.f,623.f);
+
+        sprite_button_hard_red.setTexture(button_hard_red);
+        sprite_button_hard_red.setPosition(570.f,623.f);
     }
     void dif_levels_run();
 };
--- Sources/dif_levels.cpp
+++ Sources/dif_levels.cpp
@@ -20,8 +20,32 @@ void dif_levels::dif_levels_run()
 //                    window.close();
 //                }
 //            }
-            //кнокпа сложного режима
-            //открытие нового окна при нажатии
+
+//кнокпа сложного режима
+            //подсветка красным при наведении на кнопку курсора
+            if (event.type == sf::Event::MouseMoved)
+            {
+                sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+             //если курсор наведен на кнопку
+                if (sprite_button_hard_blue.getGlobalBounds().contains(mousePosition))
+               {
+                    //замена изображения на красную кнопки
+                    window.clear(sf::Color::Black);
+                    window.draw(sprite_background);
+                    window.draw(sprite_button_hard_red);
+                    window.display();
+                }
+                else
+                {
+                    //замена изображения на голубую кнопки
+                    window.clear(sf::Color::Black);
+                    window.draw(sprite_background);
+                    window.draw(sprite_button_hard_blue);
+                    window.display();
+                }
+           }
+
+            //открытие нового окна при нажатии на данную кнопку
 //            if (event.type==sf::Event::MouseButtonPressed && event.mouseButton.button==sf::Mouse::Left)
 //            {
 //                if ((event.mouseButton.x>=570 && event.mouseButton.x<=1261) && (event.mouseButton.y>=623 && event.mouseButton.y<=738))
@@ -43,9 +67,6 @@ void dif_levels::dif_levels_run()
             }
 
         }
-        window.clear(sf::Color::Black);
-        window.draw(sprite);
-        window.display();
     }
     return;
 }
