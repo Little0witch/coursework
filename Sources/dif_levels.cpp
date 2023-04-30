11e3501eba976bd66c85d5b324a8b4bf72483ec6
tanya <eeeee.nom@gmail.com>
2023-04-30 14:13:01 +0300

    add button illumination

--- Sources/dif_levels.cpp
+++ Sources/dif_levels.cpp
@@ -11,53 +11,77 @@ void dif_levels::dif_levels_run()
             {
                 window.close();
             }
-            //кнокпа простого режима
-            //открытие нового окна при нажатии
-//            if (event.type==sf::Event::MouseButtonPressed && event.mouseButton.button==sf::Mouse::Left)
-//            {
-//                if ((event.mouseButton.x>=570 && event.mouseButton.x<=1261) && (event.mouseButton.y>=450 && event.mouseButton.y<=565))
-//                {
-//                    window.close();
-//                }
-//            }
 
-//кнокпа сложного режима
-            //подсветка красным при наведении на кнопку курсора
+            //подсветка красным при наведении курсора на кнопку
             if (event.type == sf::Event::MouseMoved)
             {
                 sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Mouse::getPosition(window));
-             //если курсор наведен на кнопку
-                if (sprite_button_hard_blue.getGlobalBounds().contains(mousePosition))
-               {
+                //кнокпа простого режима
+                //если курсор наведен на кнопку
+                if (sprite_button_easy_blue.getGlobalBounds().contains(mousePosition))
+                {
                     //замена изображения на красную кнопки
                     window.clear(sf::Color::Black);
                     window.draw(sprite_background);
-                    window.draw(sprite_button_hard_red);
+                    window.draw(sprite_button_easy_red);
+                    window.draw(sprite_button_hard_blue);
+                    window.draw(sprite_button_exit_blue);
                     window.display();
                 }
                 else
                 {
-                    //замена изображения на голубую кнопки
-                    window.clear(sf::Color::Black);
-                    window.draw(sprite_background);
-                    window.draw(sprite_button_hard_blue);
-                    window.display();
+                    //кнокпа сложного режима
+                    if (sprite_button_hard_blue.getGlobalBounds().contains(mousePosition))
+                    {
+                        //замена изображения на красную кнопки
+                        window.clear(sf::Color::Black);
+                        window.draw(sprite_background);
+                        window.draw(sprite_button_easy_blue);
+                        window.draw(sprite_button_hard_red);
+                        window.draw(sprite_button_exit_blue);
+                        window.display();
+                    }
+                    else
+                    {
+                        //кнокпа выхода
+                        if (sprite_button_exit_blue.getGlobalBounds().contains(mousePosition))
+                        {
+                            //замена изображения на красную кнопки
+                            window.clear(sf::Color::Black);
+                            window.draw(sprite_background);
+                            window.draw(sprite_button_easy_blue);
+                            window.draw(sprite_button_hard_blue);
+                            window.draw(sprite_button_exit_red);
+                            window.display();
+                        }
+                        else
+                        {
+                            //замена изображения на голубую кнопку
+                            window.clear(sf::Color::Black);
+                            window.draw(sprite_background);
+                            window.draw(sprite_button_easy_blue);
+                            window.draw(sprite_button_hard_blue);
+                            window.draw(sprite_button_exit_blue);
+                            window.display();
+                        }
+                    }
                 }
-           }
+            }
 
-            //открытие нового окна при нажатии на данную кнопку
-//            if (event.type==sf::Event::MouseButtonPressed && event.mouseButton.button==sf::Mouse::Left)
-//            {
-//                if ((event.mouseButton.x>=570 && event.mouseButton.x<=1261) && (event.mouseButton.y>=623 && event.mouseButton.y<=738))
-//                {
-//                    window.close();
-//                }
-//            }
-
-            //кнокпа выхода
             //открытие нового окна при нажатии
             if (event.type==sf::Event::MouseButtonPressed && event.mouseButton.button==sf::Mouse::Left)
             {
+//кнокпа простого режима
+                if ((event.mouseButton.x>=570 && event.mouseButton.x<=1261) && (event.mouseButton.y>=450 && event.mouseButton.y<=565))
+                {
+                    window.close();
+                }
+//кнокпа сложного режима
+                if ((event.mouseButton.x>=570 && event.mouseButton.x<=1261) && (event.mouseButton.y>=623 && event.mouseButton.y<=738))
+                {
+                    window.close();
+                }
+//кнокпа выхода
                 if ((event.mouseButton.x>=570 && event.mouseButton.x<=1261) && (event.mouseButton.y>=793 && event.mouseButton.y<=908))
                 {
                     window.close();
@@ -65,7 +89,6 @@ void dif_levels::dif_levels_run()
                     window_menu.main_menu_run();
                 }
             }
-
         }
     }
     return;
