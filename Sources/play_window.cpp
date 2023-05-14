09dfaebf7682a72c97331d46390571cc2cdabfd1
user <ser.funy@mail.ru>
2023-05-14 14:42:48 +0300

    full project

--- Sources/play_window.cpp
+++ Sources/play_window.cpp
@@ -33,8 +33,7 @@ void play_window::play_with_soft(Player &player, bool &flag) {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-            }
-            else {
+            } else {
                 if (sizeList(listOfShipsOfPlayer) == 0 && sizeList(listOfShipsOfBot) != 0) {
                     //player lose
                     window_lose windowLose(window);
@@ -343,20 +342,13 @@ void play_window::play_window_run_for_player(Player &player, bool &flag, struct 
 }
 
 //we are client
-void play_window::play_with_server(Player &player, bool &flag, struct dataOfSocket dataOfSocket) {
-
-}
-
-//we are server
-void play_window::play_with_client(Player &player, bool &flag, struct dataOfSocket data_of_socket) {
+void play_window::play_with_server(Player &player, bool &flag, struct dataOfSocket data_of_socket) {
     int hit = 0;
-    bool isServerMove = true;
-    struct coordinates coord{};
+    bool isClientMove = false;
+    struct coord coord{};
     ListOfShips listOfShipsOfPlayer = player.getListOfMyShips();
-    int serverWin = 0;//1 - win 2 - lose
-    int** enemyField = nullptr;
-    enemyField = allocateMemory(enemyField,10,10);
-    init(enemyField,10,10);
+    int clientWin = 0;//1 - win 2 - lose
+    Player server;
 
     struct threadDataOfSocket thread_data_of_socket{};
     thread_data_of_socket.socketData = data_of_socket;
@@ -364,9 +356,8 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
     thread_data_of_socket.dataOfBool.readyToPlay = false;
 
     pthread_t idOfThread;
-    int resultOfThread = pthread_create(&idOfThread, NULL, isActiveSocketThread, (void*)&thread_data_of_socket);
-    if (resultOfThread != 0)
-    {
+    int resultOfThread = pthread_create(&idOfThread, NULL, isActiveSocketThread, (void *) &thread_data_of_socket);
+    if (resultOfThread != 0) {
         printf("Error of create thread");
         exit(0);
     }
@@ -376,12 +367,169 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
+            } else {
+                if (clientWin == 2) {
+                    //player lose
+                    closeSocket(data_of_socket);
+                    window_lose windowLose(window);
+                    windowLose.window_lose_run();
+                    return;
+                }
+
+                if (clientWin == 1) {
+                    //player win
+                    closeSocket(data_of_socket);
+                    window_win windowWin(window);
+                    windowWin.window_win_run();
+                    return;
+                }
+
+                if (isClientMove) {
+                    if (event.type == sf::Event::MouseButtonPressed) {
+                        if (event.mouseButton.button == sf::Mouse::Left) {
+                            // Получение координат щелчка мыши
+                            int x = event.mouseButton.x;
+                            int y = event.mouseButton.y;
+                            if ((x >= 1085 && x <= 1650) && (y >= 285 && y <= 847) &&
+                                isEmpty(x, y, player.getEnemyField())) {
+
+                                x = (x - 1085) / 56;
+                                y = (y - 285) / 56;
+
+                                coord.x = x;
+                                coord.y = y;
+
+                                sendCoordToServer(data_of_socket.sockfd, coord);
+
+                                hit = getIsHitFromServer(data_of_socket.sockfd);
+
+                                if (hit == 9) {
+                                    clientWin = 1;
+                                }
+                                else
+                                {
+                                    player.addHit(y, x, hit);
+
+                                    if (hit == -1)
+                                        isClientMove = false;
+                                    else
+                                        isClientMove = true;
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    coord = getCoordFromServer(data_of_socket.sockfd);
+                    hit = isHit(listOfShipsOfPlayer, &listOfShipsOfPlayer, coord.y, coord.x);
+
+                    if (sizeList(listOfShipsOfPlayer) == 0) {
+                        clientWin = 2;
+                        hit = 9;
+                    } else
+                    {
+                        server.addHit(coord.y,coord.x,hit);
+
+                        sendIsHitToServer(data_of_socket.sockfd,hit);
+
+                        if (hit == -1)
+                            isClientMove = true;
+                        else
+                            isClientMove = false;
+                    }
+                }
             }
-            else {
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2f mouse_pos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                if (sprite_button_back.getGlobalBounds().contains(mouse_pos)) {
+                    flag_exit = true;
+                }
+
+            }
+
+        }
+        window.clear(sf::Color::Black);
+        window.draw(sprite_background);
+        window.draw(sprite_button_back);
+        set_sprite_of_hit(player.getEnemyField(), 0);
+        set_sprite_of_hit(server.getEnemyField(), 1);
+
+        show_hits();
+
+        if (isClientMove) {
+            window.draw(sprite_right_arrow);
+        } else {
+            window.draw(sprite_left_arrow);
+        }
+
+        show_placement();
+
+        if (thread_data_of_socket.dataOfBool.flagToExit) {
+            pthread_cancel(idOfThread);
+            closeSockets(data_of_socket);
+            return;
+        }
+
+        if (flag_exit) {
+            window_exit exit_window(window);
+            exit_window.window_exit_run(flag);
+            if (flag) {
+                pthread_cancel(idOfThread);
+                closeSockets(data_of_socket);
+                return;
+            } else {
+                flag_exit = false;
+                window.clear(sf::Color::Black);
+                window.draw(sprite_background);
+                window.draw(sprite_button_back);
+                set_sprite_of_hit(player.getEnemyField(), 0);
+                set_sprite_of_hit(server.getEnemyField(), 1);
+
+                show_hits();
+
+                if (isClientMove) {
+                    window.draw(sprite_right_arrow);
+                } else {
+                    window.draw(sprite_left_arrow);
+                }
+                show_placement();
+                window.display();
+            }
+        }
+        window.display();
+    }
+}
+
+//we are server
+void play_window::play_with_client(Player &player, bool &flag, struct dataOfSocket data_of_socket) {
+    int hit = 0;
+    bool isServerMove = true;
+    struct coord coord{};
+    ListOfShips listOfShipsOfPlayer = player.getListOfMyShips();
+    int serverWin = 0;//1 - win 2 - lose
+    Player client;
+
+
+    struct threadDataOfSocket thread_data_of_socket{};
+    thread_data_of_socket.socketData = data_of_socket;
+    thread_data_of_socket.dataOfBool.flagToExit = false;
+    thread_data_of_socket.dataOfBool.readyToPlay = false;
+
+    pthread_t idOfThread;
+    int resultOfThread = pthread_create(&idOfThread, NULL, isActiveSocketThread, (void *) &thread_data_of_socket);
+    if (resultOfThread != 0) {
+        printf("Error of create thread");
+        exit(0);
+    }
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            } else {
                 if (serverWin == 2) {
                     //player lose
                     closeSocket(data_of_socket);
-                    freeMemory(enemyField,10);
                     window_lose windowLose(window);
                     windowLose.window_lose_run();
                     return;
@@ -390,7 +538,6 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
                 if (serverWin == 1) {
                     //player win
                     closeSocket(data_of_socket);
-                    freeMemory(enemyField,10);
                     window_win windowWin(window);
                     windowWin.window_win_run();
                     return;
@@ -408,30 +555,46 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
                                 x = (x - 1085) / 56;
                                 y = (y - 285) / 56;
 
+                                coord.x = x;
+                                coord.y = y;
 
-                                hit = isHit(listOfShipsOfBot, &listOfShipsOfBot, y, x);
+                                sendCoordToClient(data_of_socket.connfd, coord);
 
-                                player.addHit(y, x, hit);
+                                hit = getIsHitFromClient(data_of_socket.connfd);
 
-                                if (hit == -1)
-                                    isServerMove = false;
+                                if (hit == 9) {
+                                    serverWin = 1;
+                                }
                                 else
-                                    isServerMove = true;
+                                {
+                                    player.addHit(y, x, hit);
+
+                                    if (hit == -1)
+                                        isServerMove = false;
+                                    else
+                                        isServerMove = true;
+                                }
                             }
                         }
                     }
                 } else {
-                    coord = botSoft.giveCoordinates();
-                    hit = isHit(listOfShipsOfPlayer, &listOfShipsOfPlayer, coord.x, coord.y);
-                    botSoft.statusGame(hit);
-                    if (hit == -1)
-                        isServerMove = true;
-                    else
-                        isServerMove = false;
-                    usleep(500000);
+                    coord = getCoordFromClient(data_of_socket.connfd);
+                    hit = isHit(listOfShipsOfPlayer, &listOfShipsOfPlayer, coord.y, coord.x);
+                    if (sizeList(listOfShipsOfPlayer) == 0) {
+                        serverWin = 2;
+                        hit = 9;
+                    } else
+                    {
+                        client.addHit(coord.y,coord.x,hit);
+
+                        sendIsHitToClient(data_of_socket.connfd,hit);
+
+                        if (hit == -1)
+                            isServerMove = true;
+                        else
+                            isServerMove = false;
+                    }
                 }
-
-
             }
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
                 sf::Vector2f mouse_pos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
@@ -446,7 +609,7 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
         window.draw(sprite_background);
         window.draw(sprite_button_back);
         set_sprite_of_hit(player.getEnemyField(), 0);
-        set_sprite_of_hit(enemyField, 1);
+        set_sprite_of_hit(client.getEnemyField(), 1);
 
         show_hits();
 
@@ -460,7 +623,6 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
 
         if (thread_data_of_socket.dataOfBool.flagToExit) {
             pthread_cancel(idOfThread);
-            freeMemory(enemyField,10);
             closeSockets(data_of_socket);
             return;
         }
@@ -469,7 +631,6 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
             window_exit exit_window(window);
             exit_window.window_exit_run(flag);
             if (flag) {
-                freeMemory(enemyField,10);
                 pthread_cancel(idOfThread);
                 closeSockets(data_of_socket);
                 return;
@@ -479,7 +640,7 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
                 window.draw(sprite_background);
                 window.draw(sprite_button_back);
                 set_sprite_of_hit(player.getEnemyField(), 0);
-                set_sprite_of_hit(enemyField, 1);
+                set_sprite_of_hit(client.getEnemyField(), 1);
 
                 show_hits();
 
@@ -494,11 +655,4 @@ void play_window::play_with_client(Player &player, bool &flag, struct dataOfSock
         }
         window.display();
     }
-
-
-
-
-
-
-
 }
