8227360b49c2a9bd7aeb50b7fbff6d5b7c657bfa
user <ser.funy@mail.ru>
2023-05-16 11:36:50 +0300

    full project

---
+++ Headers/logs.h
@@ -0,0 +1,16 @@
+#ifndef COURSEWORK_LOGS_H
+#define COURSEWORK_LOGS_H
+
+#include <fstream>
+#include <ctime>
+#include <iostream>
+
+void logError(const std::string&);
+
+
+
+
+
+
+
+#endif //COURSEWORK_LOGS_H
---
+++ Sources/logs.cpp
@@ -0,0 +1,16 @@
+#include "../Headers/logs.h"
+
+void logError(const std::string& errorMessage) {
+    std::ofstream logfile;
+    logfile.open("error.log", std::ios::app | std::ios::out);
+    if (logfile.is_open()) {
+        std::time_t currentTime = std::time(nullptr);
+        std::string timestamp = std::asctime(std::localtime(&currentTime));
+        timestamp = timestamp.substr(0, timestamp.length() - 1);
+
+        logfile << "[" << timestamp << "] " << errorMessage << std::endl;
+
+        logfile.close();
+    }
+}
+
